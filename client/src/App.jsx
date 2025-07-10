e1c864bfb4545ec4a2399d4cd042384a24dc719e
MEHARUNISA13 <mnisa6494@gmail.com>
2025-07-11 01:15:40 +0500

    LEARNING MERGECONFLICTS

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,7 +6,7 @@ export default function App() {
   return (
    <>
    <Signup></Signup>
-   <h1>Making Change to learn pull requests</h1>
+   <h1>Making Change to learn pull requests,merge conflicts</h1>
    <h1>Hello GitHub from Mehar</h1>
    </>
    
